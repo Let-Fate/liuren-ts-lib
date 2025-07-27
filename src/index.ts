f1db1459b4e67a8c29c6742d2a53c52043e14bb0
Coaixy <Coaixy@outlook.com>
2025-07-27 14:21:14 +0800

    1.02

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,6 @@ import { getDunGan } from "./utils/dunGan";
 import { fillSanChuan, getSanChuan } from "./utils/sanChuan";
 import { getSiKe } from "./utils/siKe";
 import { getTianDiPan } from "./utils/tianDiPan";
-
-const showCopyRight = () => {
-    const styles = [
-        'color: #42c892',
-        'font-size: 14px',
-        'font-family: sans-serif',
-        'font-weight: bold',
-    ].join(';');
-    console.log("%cBy: https://github.com/let-fate/liuren-ts-lib", styles)
-}
-
-showCopyRight()
-
 export const getLiuRenByDate = (time: Date) => {
     const date = getDateByObj(time)
     const riGan = date.bazi.split(" ")[2].substring(0, 1)
@@ -49,4 +36,3 @@ export const getLiuRenBySiZhu = (year: string, month: string, day: string, hour:
     }
     return result
 }
-console.log(getLiuRenByDate(new Date("2025-07-27 0:00:00")))
